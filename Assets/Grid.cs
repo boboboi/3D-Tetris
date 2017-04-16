d3fcc4966f740e41a3cb79b50ce2a4479ac61cb9
佐原慎也 <yeah.boboboi@gmail.com>
2017-04-16 11:21:05 +0900

    3D-Tetris 標準テトリス

---
+++ Assets/Grid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+	
+	// The Grid itself
+	public static int w = 10;
+	public static int h = 20;
+	public static int z = 0;
+	public static Transform[,,] grid = new Transform[w, h, z+1];
+
+	public static Vector3 roundVec3(Vector3 v) {
+		return new Vector3(Mathf.Round(v.x), Mathf.Round(v.y), Mathf.Round(v.z));
+	}
+
+	public static bool insideBorder(Vector3 pos) {
+		return ((int)pos.x >= 0 &&
+			(int)pos.x < w &&
+			(int)pos.y >= 0 &&
+			(int)pos.z == 0 );
+	}
+
+	public static void deleteRow(int y) {
+		for (int x = 0; x < w; ++x) {
+			Destroy(grid[x, y, z].gameObject);
+			grid[x, y, z] = null;
+		}
+	}
+
+	public static void decreaseRow(int y) {
+		for (int x = 0; x < w; ++x) {
+			if (grid[x, y, z] != null) {
+				// Move one towards bottom
+				grid[x, y-1, z] = grid[x, y, z];
+				grid[x, y, z] = null;
+
+				// Update Block position
+				grid[x, y-1, z].position += new Vector3(0, -1, 0);
+			}
+		}
+	}
+
+	public static void decreaseRowsAbove(int y) {
+		for (int i = y; i < h; ++i)
+			decreaseRow(i);
+	}
+
+	public static bool isRowFull(int y) {
+		for (int x = 0; x < w; ++x)
+			if (grid[x, y, z] == null)
+				return false;
+		return true;
+	}
+
+	public static void deleteFullRows() {
+		for (int y = 0; y < h; ++y) {
+			if (isRowFull(y)) {
+				deleteRow(y);
+				decreaseRowsAbove(y+1);
+				--y;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	// Groups
+	public GameObject[] groups;
+
+	public void spawnNext() {
+		// Random Index
+		int i = Random.Range(0, groups.Length);
+
+		// Spawn Group at current Position
+		Instantiate(groups[i],
+			transform.position,
+			Quaternion.identity);
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Spawn initial Group
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
